f46bcb3719342e3ec90b7bcb290e7544c1b521b7
RaymondChoi54 <concept_ray@hotmail.com>
2018-05-01 15:46:29 -0400

    Fixed errors from MTA feed

--- backend/app.js
+++ backend/app.js
@@ -6,18 +6,40 @@ const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const request = require('request');
 
 const feed_ids = [1, 26, 16, 21, 2, 11, 31, 36, 51]
-const lastGoodFeeds = []
+var lastGoodFeeds = {}
 
 function timeCheck(ms) {
     var difference = ms - new Date().getTime();
     return difference >= 0 && difference <= 1800000;
 }
 
+function feedToStops(feed) {
+    feed.entity.forEach(function(entity) {
+        if(entity.trip_update) {
+            for(var j = 0; j < entity.trip_update.stop_time_update.length; j++) {
+                var time = 0
+                if(entity.trip_update.stop_time_update[j].arrival && timeCheck(entity.trip_update.stop_time_update[j].arrival.time.low * 1000)) {
+                    time = entity.trip_update.stop_time_update[j].arrival.time.low * 1000
+                
+                    req = {body: 
+                            {
+                                route_id: entity.trip_update.trip.route_id,
+                                stop_id: entity.trip_update.stop_time_update[j].stop_id.substring(0, 3),
+                                direction: entity.trip_update.stop_time_update[j].stop_id.substring(3, 4),
+                                arrival: time
+                            }}
+                    trainStops.addStop(req)
+                }
+            }
+        }
+    });
+}
+
 function updater() {
     trainStops.destroy()
     console.log("Updating")
     for(var i = 0; i < feed_ids.length; i++) {
-        console.log("Feed " + feed_ids[i])
+        let value = i
         var requestSettings = {
             method: 'GET',
             url: 'http://datamine.mta.info/mta_esi.php?key=db1994ff578c44c29bbe97eee6de1076&feed_id=' + feed_ids[i],
@@ -26,29 +48,13 @@ function updater() {
         request(requestSettings, function (error, response, body) {
             try {
                 if(!error && response.statusCode == 200) {
-                    var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-                    feed.entity.forEach(function(entity) {
-                        if (entity.trip_update) {
-                            for(var j = 0; j < entity.trip_update.stop_time_update.length; j++) {
-                                var time = 0
-                                if(entity.trip_update.stop_time_update[j].arrival && timeCheck(entity.trip_update.stop_time_update[j].arrival.time.low * 1000)) {
-                                    time = entity.trip_update.stop_time_update[j].arrival.time.low * 1000
-                                
-                                    req = {body: 
-                                            {
-                                                route_id: entity.trip_update.trip.route_id,
-                                                stop_id: entity.trip_update.stop_time_update[j].stop_id.substring(0, 3),
-                                                direction: entity.trip_update.stop_time_update[j].stop_id.substring(3, 4),
-                                                arrival: time
-                                            }}
-                                    trainStops.addStop(req)
-                                }
-                            }
-                        }
-                    });
+                    var feed = GtfsRealtimeBindings.FeedMessage.decode(body)
+                    feedToStops(feed)
+                    lastGoodFeeds[feed_ids[value]] = feed
                 }
             } catch(exception) {
-                console.log("Error: Request Error")
+                console.log("Error: Request Error on feed " + feed_ids[value])
+                feedToStops(lastGoodFeeds[feed_ids[value]])
             }
         });
     }
@@ -56,7 +62,7 @@ function updater() {
 
 updater();
 
-var minutes = 1
+var minutes = 0.1
 the_interval = minutes * 60 * 1000;
 setInterval(function() {
     updater()
